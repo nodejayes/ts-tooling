96b3dcd2a0ae85c02d93d5456fd4bbe593930ba9
markus <markusgilg@outlook.de>
2019-10-26 14:17:41 +0200

    add bool test

---
+++ spec/primitive/bool.spec.ts
@@ -0,0 +1,19 @@
+import {assert} from 'chai';
+import {Bool} from "../../src/primitive/bool";
+
+describe('Bool Tests', () => {
+    it('can create Boolean Value', () => {
+        assert.isTrue(new Bool(true).Value);
+        assert.isFalse(new Bool(false).Value);
+    });
+    it('can check Boolean Value', () => {
+        assert.isTrue(new Bool(true).IsTrue());
+        assert.isFalse(new Bool(false).IsTrue());
+        assert.isTrue(new Bool(false).IsFalse());
+        assert.isFalse(new Bool(true).IsFalse());
+    });
+    it('can switch Boolean Value', () => {
+        assert.isTrue(new Bool(false).Switch().Value);
+        assert.isFalse(new Bool(true).Switch().Value);
+    });
+});
