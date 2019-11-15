418595485bab12899d99a2c599e730d2d55021c7
Markus Gilg <markusgilg@outlook.de>
2019-11-15 13:50:14 +0100

    Documentation

--- src/pattern/dispose/using.ts
+++ src/pattern/dispose/using.ts
@@ -2,8 +2,21 @@ import {IDisposable} from './disposable';
 
 /**
  * use a Instance and Dispose it after Execution
- * @param item
- * @param cb
+ * @param item a instance of a Class to Dispose after running the using section
+ * @param cb what is to do in this using?
+ *
+ * ```typescript
+ * class WithDisposable implements IDisposable {
+ *   Name = 'WithoutDisposable';
+ *
+ *   Dispose(): void {
+ *     this.Name = '';
+ *   }
+ * }
+ * using(WithDisposable, (i) => {
+ *   // Do whatever you want to do with the new Instance of the Class
+ * });
+ * ```
  */
 export function using<T extends IDisposable>(item: new () => T, cb: (d: T) => void): void {
     const tmp = new item();
--- src/pattern/events/event.handler.ts
+++ src/pattern/events/event.handler.ts
@@ -3,7 +3,7 @@ import {Dictionary} from '../../complex/dictionary';
 import {StringFactory} from '../../utils/string.factory';
 
 /**
- * represent a Event Handler
+ * lets create a Event Handler you can subscribe or unsubscribe
  */
 export class EventHandler<T> {
     private _stream = new Subject<T>();
@@ -12,7 +12,12 @@ export class EventHandler<T> {
     /**
      * invoke the Event on the Handler
      * @param args
-     * @constructor
+     *
+     * ```typescript
+     * const handler = new EventHandler<number>();
+     * // sends 1 to every Subscriber
+     * handler.Invoke(1);
+     * ```
      */
     Invoke(args: T): void {
         this._stream.next(args);
@@ -23,7 +28,14 @@ export class EventHandler<T> {
      * @param key the key to identify the subscription
      * @param cb
      * @returns the Idx of the Subscription
-     * @constructor
+     *
+     * ```typescript
+     * const handler = new EventHandler<number>();
+     * handler.Subscribe('X', (i) => {
+     *     // get the Number that was send by a Invoke call (2)
+     * });
+     * handler.Invoke(2);
+     * ```
      */
     Subscribe(key: string, cb: (d: T) => void) {
         this._subscriptions.Add(key, this._stream.subscribe(cb));
@@ -32,7 +44,18 @@ export class EventHandler<T> {
     /**
      * unsubscribe all callbacks
      * @param key the key to identify the Subscription to unsubscribe
-     * @constructor
+     *
+     * ```typescript
+     * const handler = new EventHandler<number>();
+     * handler.Subscribe('X', (i) => {
+     *      // nothing happen here while the Handler was unsubscribe
+     * });
+     * // unsubscribe only the X Subscriber
+     * handler.Unsubscribe('X');
+     * // unsubscribe all Subscriber
+     * handler.Unsubscribe();
+     * handler.Invoke(2);
+     * ```
      */
     Unsubscribe(key?: string) {
         if (StringFactory.IsNullOrEmpty(key)) {
