721f49551fb7dfe72f4a869f20fc91f511b55024
Markus Gilg <markusgilg@outlook.de>
2019-11-15 13:28:37 +0100

    Documentation: StopWatch

--- src/utils/stopwatch.ts
+++ src/utils/stopwatch.ts
@@ -1,27 +1,60 @@
+// Nanoseconds in one Second
 const NS_PER_SEC = 1e9;
 
 /**
- * a Stopwatch to measure the Time in ms
- * the StopWatch starts in the Constructor
- * You can measure multiple Times when you give the StopWatch a key
+ * measure the Time between Code Lines in ms
+ * the StopWatch starts when a new Instance was created and can give the Elapsed ms when ElapsedMs was called.
+ * measure multiple Times is also possible with SectionStart and SectionElapsedMs
  */
 export class StopWatch {
-    private _time: number | [number, number];
-    private _multipleTimes: {[key: string]: number | [number, number]};
+    private readonly _time: number | [number, number];
+    private readonly _multipleTimes: {[key: string]: number | [number, number]};
 
+    /**
+     * create a new StopWatch Instance at this Time the StopWatch was started
+     *
+     * ```typescript
+     * const watch = new StopWatch();
+     * // returns the elapsed Ms from construction and this Line
+     * watch.ElapsedMs();
+     * ```
+     */
     constructor() {
         this._time = this.getTimestamp();
         this._multipleTimes = {};
     }
 
+    /**
+     * starts the StopWatch for a specific Section marks by the given key
+     * @param key the key that indicates the Section
+     *
+     * ```typescript
+     * const watch = new StopWatch();
+     * watch.SectionStart('A');
+     * // logs the Time between SectionStart('A') and SectionElapsedMs('A')
+     * watch.SectionElapsedMs('A');
+     * watch.SectionStart('B');
+     * // logs the Time between SectionStart('B') and SectionElapsedMs('B')
+     * watch.SectionElapsedMs('B');
+     * // logs the Time between SectionStart('A') and this Line with SectionStart('B') and SectionElapsedMs('B')
+     * watch.SectionElapsedMs('A');
+     * ```
+     */
     public SectionStart(key: string): void {
         this._multipleTimes[key] = this.getTimestamp();
     }
 
+    /**
+     * get the Time in ms Elapsed by the Section matches the given key
+     * @param key the key that indicates the Section
+     */
     public SectionElapsedMs(key: string): number {
         return this.getMultipleTimeDiff(key);
     }
 
+    /**
+     * gets the Elapsed Time in ms from the StopWatch
+     */
     public ElapsedMs(): number {
         return this.getTimeDiff();
     }
