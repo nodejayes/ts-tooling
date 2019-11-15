45682b33025f973d576d1df3ff2e94640681888f
Markus Gilg <markusgilg@outlook.de>
2019-11-15 14:09:41 +0100

    begin ByteStream

---
+++ src/complex/byte.stream.ts
@@ -0,0 +1,10 @@
+export class ByteStream {
+    private readonly _value: number[] = [];
+    private _position = 0;
+
+    Write() {}
+    WriteByte() {}
+    Read() {}
+    ReadByte() {}
+    ToString() {}
+}
