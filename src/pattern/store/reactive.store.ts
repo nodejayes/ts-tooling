03a609178313005b8f3367421d1e119ef951b450
Markus Gilg <markusgilg@outlook.de>
2020-01-22 20:01:50 +0100

    v2.4.5

--- src/pattern/store/reactive.store.ts
+++ src/pattern/store/reactive.store.ts
@@ -68,7 +68,13 @@ export class ReactiveStore<T> {
 
     /**
      * listen to a specific Property or a complete State change
-     * @param selector
+     * you can use the SafeBehaviorSubject same as a BehaviorSubject but follow Functions has no effect
+     * next, complete, error
+     * @param selector select the Part of the State to Listen
+     *
+     * @example
+     * // write the data Property of the State into the console
+     * store.Listen(s => s.data).subscribe(d => console.info(d));
      */
     Listen<K>(selector: (d: T) => K): SafeBehaviorSubject<K> {
         let key = this.parseSelectorAccess(selector);
@@ -82,8 +88,20 @@ export class ReactiveStore<T> {
 
     /**
      * mutate a specific Property or a complete State
-     * @param selector
-     * @param mutation
+     * @param selector select the Part of the State to Mutate
+     * @param mutation define how to change the State
+     *
+     * @example
+     * // simple Example to set the State
+     * store.Mutate(s => s.data, () => newData);
+     *
+     * @example
+     * // make a complex Mutation
+     * store.Mutate(s => s, old => {
+     *     old.data = [];
+     *     old.loading = true;
+     *     return old;
+     * });
      */
     Mutate<K>(selector: (d: T) => K, mutation: (s: K) => K): void {
         const key = this.parseSelectorAccess(selector);
