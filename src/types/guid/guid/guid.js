18bced3133697f23f35948133f6bc7ce8560cb18
Markus Gilg <markusgilg@outlook.de>
2020-03-07 12:14:40 +0100

    documentation

--- src/types/guid/guid/guid.js
+++ src/types/guid/guid/guid.js
@@ -1,11 +1,43 @@
 const {StringFactory} = require('../../string');
 const {ValidateGuid, Uuidv4} = require('../../../core/guid/guid');
 
+/**
+ * represent the Global Uniqe Identifier
+ *
+ * @memberof module:types/guid
+ */
 class Guid {
+    /**
+     * is this Guid a Empty Guid
+     *
+     * @returns {boolean} matches the empty guid
+     *
+     * @example
+     * // returns true
+     * new Guid('00000000-0000-0000-0000-000000000000').IsEmpty();
+     * // returns false
+     * new Guid('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c').IsEmpty();
+     */
     get IsEmpty() {
         return this._value.Equals(Guid.Empty.ToString());
     }
 
+    /**
+     * create a new Guid
+     *
+     * generates a new one when no guid was passed
+     *
+     * @constructor
+     *
+     * @param guid {string?} a new guid as string representation
+     * @return {Guid}
+     *
+     * @example
+     * // returns a new generated Guid
+     * new Guid();
+     * // returns "6bcb9d2c-ae48-4310-8d56-ea7accffcc8c"
+     * new Guid('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c');
+     */
     constructor(guid) {
         this._value = null;
         if (StringFactory.IsNullOrEmpty(guid)) {
@@ -18,10 +50,32 @@ class Guid {
         }
     }
 
+    /**
+     * converts the Guid to a String representation
+     *
+     * @returns Guid {Guid} as string
+     *
+     * @example
+     * // returns "6bcb9d2c-ae48-4310-8d56-ea7accffcc8c"
+     * new Guid('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c').ToString();
+     */
     ToString() {
         return this._value;
     }
 
+    /**
+     * check if the Guid is Equal another Guid
+     *
+     * @param guid {Guid} the guid to check
+     * @returns {boolean} are the Guid´s equal or not
+     *
+     * @example
+     * // returns true
+     * new Guid('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c').Equals('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c');
+     * new Guid('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c').Equals(new Guid('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c'));
+     * // returns false
+     * new Guid('6bcb9d2c-ae48-4310-8d56-ea7accffcc8c').Equals('4fa89189-03b5-43f2-b184-8a42adeebfe7');
+     */
     Equals(guid) {
         if (guid instanceof Guid) {
             return this._value.Equals(guid.ToString());
@@ -30,6 +84,19 @@ class Guid {
     }
 }
 
+/**
+ * get a empty Guid
+ *
+ * @memberof module:types/guid.Guid
+ * @property Empty
+ * @readonly
+ * @static
+ * @returns {Guid} the empty guid
+ *
+ * @example
+ * // returns "00000000-0000-0000-0000-000000000000"
+ * Guid.Empty;
+ */
 Object.defineProperty(Guid, "Empty", {
     get: function () {
         return new Guid('00000000-0000-0000-0000-000000000000');
@@ -38,6 +105,20 @@ Object.defineProperty(Guid, "Empty", {
     configurable: true
 });
 
+/**
+ * validate a Guid
+ *
+ * @memberof module:types/guid.Guid
+ * @static
+ * @param guid {Guid} the guid to validate
+ * @returns {boolean} is the given guid string valid or not
+ *
+ * @example
+ * // returns true
+ * Guid.Validate('00000000-0000-0000-0000-000000000000');
+ * // returns false
+ * Guid.Validate('00000000000000000000000000000000');
+ */
 Guid.Validate = (guid) => {
     return ValidateGuid(guid);
 };
--- src/types/guid/index.js
+++ src/types/guid/index.js
@@ -1,3 +1,8 @@
+/**
+ * add a Guid DataType
+ *
+ * @module types/guid
+ */
 const {Guid} = require('./guid/guid');
 
 module.exports = {Guid};
