3ddef4b443ec7810d0bc76b1e6e894bab41129d8
Markus Gilg <markusgilg@outlook.de>
2020-03-07 15:10:10 +0100

    documentation

--- src/types/object/factory/object.factory.js
+++ src/types/object/factory/object.factory.js
@@ -1,16 +1,71 @@
 const {Merge, RecursiveDeepCopy} = require('../../../core/object/object');
 const sizeof = require('object-sizeof');
 
+/**
+ * Object Utils Class to perform some Operations on Objects
+ *
+ * @memberof module:types/object
+ */
 class ObjectFactory {}
 
+/**
+ * copy a Object Instance and get a new one
+ *
+ * @memberof module:types/object.ObjectFactory
+ * @static
+ * @param instance {any} the object to copy
+ * @returns {any} the new Object instance
+ *
+ * @example
+ * // returns {Hello:'World!'}
+ * ObjectFactory.Copy({Hello:'World!'});
+ * // returns false
+ * const obj = {Hello:'World!'};
+ * obj === ObjectFactory.Copy(obj);
+ */
 ObjectFactory.Copy = (instance) => {
     return RecursiveDeepCopy(instance);
 };
 
+/**
+ * check if the given Object has some self references
+ *
+ * @memberof module:types/object.ObjectFactory
+ * @static
+ * @param obj {any} the Object to check
+ * @return {boolean}
+ *
+ * @example
+ * const obj1 = {hello:'world'};
+ * const obj2 = {hello:'world'};
+ * obj2.ref = obj2;
+ * // returns false while the Object has no self references
+ * ObjectFactory.IsCircular(obj1);
+ * // returns true while obj2 has a self reference on the ref Property
+ * ObjectFactory.IsCircular(obj2);
+ */
 ObjectFactory.IsCircular = (obj) => {
     return ObjectFactory.GetCircular(obj).length > 0;
 };
 
+/**
+ * returns the Keys of self references in Objects
+ *
+ * @memberof module:types/object.ObjectFactory
+ * @static
+ * @param obj {any} the Object to check
+ * @return {string[]}
+ *
+ * @example
+ * const obj1 = {hello:'world',c:null};
+ * const obj2 = {test:'me',c:obj1};
+ * obj1.c = obj2;
+ * const combined = {t1:obj1,t2:obj2,t3:null};
+ * const obj3 = combined;
+ * combined.t3 = obj3;
+ * // returns ['c', 'c', 't1']
+ * ObjectFactory.GetCircular(combined);
+ */
 ObjectFactory.GetCircular = (obj) => {
     const alreadyChecked = [];
     const keyReferences = [];
@@ -34,10 +89,44 @@ ObjectFactory.GetCircular = (obj) => {
     return check(obj);
 };
 
+/**
+ * returns the Size in Byte of a Object Instance
+ *
+ * @memberof module:types/object.ObjectFactory
+ * @static
+ * @param obj {any} the Object to check
+ * @return {number}
+ *
+ * @example
+ * // returns 22
+ * ObjectFactory.SizeOf({Hello: 'World!'});
+ */
 ObjectFactory.SizeOf = (obj) => {
     return sizeof(obj);
 };
 
+/**
+ * merge the child into the parent object
+ *
+ * @memberof module:types/object.ObjectFactory
+ * @static
+ * @param parent {any}
+ * @param child {any}
+ * @return {any}
+ *
+ * @example
+ * // returns {name:'muster',age:10,state:{active:true}}
+ * const obj1 = {
+ *      name:'muster',
+ *      age:10,
+ * };
+ * const obj2 = {
+ *      state:{
+ *          active:true
+ *      }
+ * };
+ * ObjectFactory.Merge(obj1, obj2);
+ */
 ObjectFactory.Merge = (parent, child) => {
     return Merge(parent, child)
 };
--- src/types/object/index.js
+++ src/types/object/index.js
@@ -1,3 +1,9 @@
+/**
+ * some util Functions for Objects
+ *
+ * @module types/object
+ */
+
 const {ObjectFactory} = require('./factory/object.factory');
 
 module.exports = {ObjectFactory};
